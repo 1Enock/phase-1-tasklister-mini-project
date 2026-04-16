e9d0a0e3016a8eba8e2edadc0833e7cf17e521d2
Enock <enock.kibet@student.moringaschool.com>
2026-04-16 11:48:53 +0300

    Added the script and modified the html file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("task-form");
-const input = document.getElementById("new-task");
-const list = document.getElementById("task-list");
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const task = input.value.trim();
-
-    // IMPORTANT: ensure test sees text
-    const li = document.createElement("li");
-    li.textContent = task;
-
-    list.appendChild(li);
-
-    input.value = "";
-});// your code here
+  // your code here
 });
